0ea84b9951974829520a162306fb33822ba96908
Hoang Anh Tuan <tuanhase180764@fpt.edu.vn>
2025-06-16 22:25:21 +0700

    update project staff

---
+++ HIVTreatmentAndMedicalServicesSystem/HIVTreatment/Controllers/LoginController.cs
@@ -0,0 +1,45 @@
+﻿using HIVTreatment.DTOs;
+using HIVTreatment.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace HIVTreatment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public LoginController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        
+        
+        // Add logout endpoint
+        [HttpPost("logout")]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return Ok("Đăng xuất thành công");
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginDTO loginDTO)
+        {
+            if (string.IsNullOrEmpty(loginDTO.Email) || string.IsNullOrEmpty(loginDTO.Password))
+                return BadRequest("Email và mật khẩu không được để trống");
+
+            var result = _userService.Login(loginDTO.Email, loginDTO.Password);
+            if (result == null)
+                return Unauthorized("Sai email hoặc mật khẩu");
+
+            return Ok(result);
+        }
+
+    }
+
+}
---
+++ HIVTreatmentAndMedicalServicesSystem/HIVTreatment/Repositories/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using HIVTreatment.Models;
+
+namespace HIVTreatment.Repositories
+{
+    public interface IUserRepository
+    {
+        User GetByEmail(string email);
+        User GetLastUser();
+        void Add(User user);
+        bool EmailExists(string email);
+        User GetByUserId(string UserId);
+        void Update(User user);
+    }
+}
