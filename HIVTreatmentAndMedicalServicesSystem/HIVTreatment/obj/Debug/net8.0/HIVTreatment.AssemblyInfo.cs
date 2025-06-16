781c04d401cf472ac0705c8792a68f8dfc7e22a1
lequocviet3103 <vietlqse180751@fpt.edu.vn>
2025-06-16 23:17:29 +0700

    login success

--- HIVTreatmentAndMedicalServicesSystem/HIVTreatment/obj/Debug/net8.0/HIVTreatment.AssemblyInfo.cs
+++ HIVTreatmentAndMedicalServicesSystem/HIVTreatment/obj/Debug/net8.0/HIVTreatment.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HIVTreatment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7956311b235a0c35ee0847b9546720b22d6e889")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d5927c2ab4b4d47eea9dc3a483bf33e71a842e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("HIVTreatment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HIVTreatment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
