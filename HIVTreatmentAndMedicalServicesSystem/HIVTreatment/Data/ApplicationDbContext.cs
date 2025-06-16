226251f93560849e24a2e986eb31d5cee7ae164a
lequocviet3103 <vietlqse180751@fpt.edu.vn>
2025-06-17 00:46:45 +0700

    update login jwt token

--- HIVTreatmentAndMedicalServicesSystem/HIVTreatment/Data/ApplicationDbContext.cs
+++ HIVTreatmentAndMedicalServicesSystem/HIVTreatment/Data/ApplicationDbContext.cs
@@ -12,8 +12,8 @@ namespace HIVTreatment.Data
 
         public DbSet<User> Users { get; set; }
         public DbSet<BooksAppointment> BooksAppointments { get; set; }
-        public DbSet<Patient> Patients { get; set; }
-        public DbSet<Doctor> Doctors { get; set; }
+        //public DbSet<Patient> Patients { get; set; }
+        //public DbSet<Doctor> Doctors { get; set; }
 
     }
 }
--- HIVTreatmentAndMedicalServicesSystem/HIVTreatment/Repositories/IPatientRepository.cs
+++ HIVTreatmentAndMedicalServicesSystem/HIVTreatment/Repositories/IPatientRepository.cs
@@ -1,13 +1,1 @@
-﻿using HIVTreatment.Models;
-
-namespace HIVTreatment.Repositories
-{
-    public interface IPatientRepository
-    {
-        Patient GetByPatientId(string patientID);
-
-        Patient GetLastPatientId();
-        void Add(Patient patient);
-        void Update(Patient patient);
-    }
-}
+﻿
