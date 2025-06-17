d6274e553ba1c5521709579c3f1da4bf24194a9c
Hoang Anh Tuan <tuanhase180764@fpt.edu.vn>
2025-06-17 21:34:44 +0700

    authorize for staff

--- HIVTreatmentAndMedicalServicesSystem/HIVTreatment/obj/Debug/net8.0/HIVTreatment.AssemblyInfo.cs
+++ HIVTreatmentAndMedicalServicesSystem/HIVTreatment/obj/Debug/net8.0/HIVTreatment.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HIVTreatment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11f189248df361a8372f76c1d51754f76f2b09ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+603d3e753fcbcd1444f30bd0989f25e4c7764249")]
 [assembly: System.Reflection.AssemblyProductAttribute("HIVTreatment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HIVTreatment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
