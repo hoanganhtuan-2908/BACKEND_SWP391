50036b7654ea8c616cc0dd153fb5743d02b25322
lequocviet3103 <vietlqse180751@fpt.edu.vn>
2025-06-23 22:42:01 +0700

    update doctor id in get all doctors id

--- HIVTreatmentAndMedicalServicesSystem/HIVTreatment/DTOs/InfoDoctorDTO.cs
+++ HIVTreatmentAndMedicalServicesSystem/HIVTreatment/DTOs/InfoDoctorDTO.cs
@@ -3,6 +3,7 @@
     public class InfoDoctorDTO
     {
         public string UserID { get; set; }
+        public string DoctorId { get; set; }
         public string Fullname { get; set; }
         public string Email { get; set; }
         public string Specialization { get; set; }
--- HIVTreatmentAndMedicalServicesSystem/HIVTreatment/obj/Debug/net8.0/HIVTreatment.AssemblyInfo.cs
+++ HIVTreatmentAndMedicalServicesSystem/HIVTreatment/obj/Debug/net8.0/HIVTreatment.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HIVTreatment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed30631608801c0f037db413cca1fde77105773d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dbf9b0f610690e1ccf7293a4c0c0d76ac5b6407")]
 [assembly: System.Reflection.AssemblyProductAttribute("HIVTreatment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HIVTreatment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
