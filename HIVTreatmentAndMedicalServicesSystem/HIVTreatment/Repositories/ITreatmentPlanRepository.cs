66f9c11b05790147775c7856ee7f4c8e6a1c9d15
Hoang Anh Tuan <tuanhase180764@fpt.edu.vn>
2025-06-26 22:17:25 +0700

    Doctor and Admin get all treatment plan

---
+++ HIVTreatmentAndMedicalServicesSystem/HIVTreatment/Repositories/ITreatmentPlanRepository.cs
@@ -0,0 +1,10 @@
+﻿using HIVTreatment.Models;
+
+namespace HIVTreatment.Repositories
+{
+    public interface ITreatmentPlanRepository
+    {
+        List<TreatmentPlan> GetAll();
+        List<TreatmentPlan> GetByPatient(string patientId);
+    }
+}
---
+++ HIVTreatmentAndMedicalServicesSystem/HIVTreatment/Repositories/TreatmentPlanRepository.cs
@@ -0,0 +1,27 @@
+﻿using HIVTreatment.Data;
+using HIVTreatment.Models;
+
+namespace HIVTreatment.Repositories
+{
+    public class TreatmentPlanRepository : ITreatmentPlanRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TreatmentPlanRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<TreatmentPlan> GetAll()
+        {
+            return _context.TreatmentPlans.ToList();
+        }
+
+        public List<TreatmentPlan> GetByPatient(string patientId)
+        {
+            return _context.TreatmentPlans
+                .Where(p => p.PatientID == patientId)
+                .ToList();
+        }
+    }
+}
