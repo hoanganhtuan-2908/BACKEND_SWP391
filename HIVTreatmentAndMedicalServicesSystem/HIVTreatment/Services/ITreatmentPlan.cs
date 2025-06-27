cacc85edf476290e5d6ccc84cba28ac138ebacf1
lequocviet3103 <vietlqse180751@fpt.edu.vn>
2025-06-27 09:00:22 +0700

    creat and update treatment plan

---
+++ HIVTreatmentAndMedicalServicesSystem/HIVTreatment/Services/ITreatmentPlan.cs
@@ -0,0 +1,12 @@
+﻿using HIVTreatment.DTOs;
+using HIVTreatment.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HIVTreatment.Services
+{
+    public interface ITreatmentPlan
+    {
+        public bool AddTreatmentPlan(TreatmentPlanDTO treatmentPlanDTO);
+        public bool UpdateTreatmentPlan(UpdateTreatmentPlanDTO updateTreatmentPlanDTO);
+    }
+}
