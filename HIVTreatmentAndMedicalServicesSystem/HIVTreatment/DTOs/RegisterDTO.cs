c3a3ca1c76fdc14e6df8a9a74dcb83a5b53d437f
lequocviet3103 <vietlqse180751@fpt.edu.vn>
2025-07-03 13:51:29 +0700

    update image

--- HIVTreatmentAndMedicalServicesSystem/HIVTreatment/DTOs/RegisterDTO.cs
+++ HIVTreatmentAndMedicalServicesSystem/HIVTreatment/DTOs/RegisterDTO.cs
@@ -7,6 +7,6 @@
         public string Password { get; set; }
         public string Email { get; set; }
         public string Address { get; set; }
-        public string Image { get; set; }
+        //public string Image { get; set; } 
     }
 }
