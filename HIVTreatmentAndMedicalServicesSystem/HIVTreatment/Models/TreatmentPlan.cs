f5ecd78610732664150fa36e4ae8be2200e6dec0
Hoang Anh Tuan <tuanhase180764@fpt.edu.vn>
2025-07-04 02:05:35 +0700

    Update TreatmentPlan.cs

--- HIVTreatmentAndMedicalServicesSystem/HIVTreatment/Models/TreatmentPlan.cs
+++ HIVTreatmentAndMedicalServicesSystem/HIVTreatment/Models/TreatmentPlan.cs
@@ -14,9 +14,6 @@ namespace HIVTreatment.Models
         public string TreatmentResult { get; set; }
 
         public Patient Patient { get; set; }
-<<<<<<< HEAD
-        public Doctor Doctor { get; set; }
-=======
->>>>>>> ed5c390 (update TP show full info Patient)
+    
     }
 }
